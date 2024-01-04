1e32a8c974326a74071284623a9c3bd17a13cd81
anisayp17 <88603180+anisayp17@users.noreply.github.com>
2024-01-04 16:10:02 +0700

    Update hungarian.py

--- hungarian.py
+++ hungarian.py
@@ -104,19 +104,19 @@ st.set_page_config(
 )
 
 st.title("Hungarian Heart Disease")
-st.write(f"**_Model's Accuracy_** :  :green[**{accuracy}**]% (:red[_Do not copy outright_])")
+st.write(f"**_Model's Accuracy_** :  :#ADFF2F[**{accuracy}**]% (:#E32227[_Do not copy outright_])")
 st.write("")
 
 tab1, tab2, tab3 = st.tabs(["Single-predict", "Multi-predict", "Description"])
 
 with tab1:
-  st.sidebar.header("**User Input** Sidebar")
+  st.sidebar.header("**User Input**")
 
-  age = st.sidebar.number_input(label=":violet[**Age**]", min_value=df_final['age'].min(), max_value=df_final['age'].max())
-  st.sidebar.write(f":orange[Min] value: :orange[**{df_final['age'].min()}**], :red[Max] value: :red[**{df_final['age'].max()}**]")
+  age = st.sidebar.number_input(label=":#4682B4[**Age**]", min_value=df_final['age'].min(), max_value=df_final['age'].max())
+  st.sidebar.write(f":gold[Min] value: :#B8860B[**{df_final['age'].min()}**], :#E32227[Max] value: :red[**{df_final['age'].max()}**]")
   st.sidebar.write("")
 
-  sex_sb = st.sidebar.selectbox(label=":violet[**Sex**]", options=["Male", "Female"])
+  sex_sb = st.sidebar.selectbox(label=":#4682B4[**Sex**]", options=["Male", "Female"])
   st.sidebar.write("")
   st.sidebar.write("")
   if sex_sb == "Male":
@@ -126,7 +126,7 @@ with tab1:
   # -- Value 0: Female
   # -- Value 1: Male
 
-  cp_sb = st.sidebar.selectbox(label=":violet[**Chest pain type**]", options=["Typical angina", "Atypical angina", "Non-anginal pain", "Asymptomatic"])
+  cp_sb = st.sidebar.selectbox(label=":#4682B4[**Chest pain type**]", options=["Typical angina", "Atypical angina", "Non-anginal pain", "Asymptomatic"])
   st.sidebar.write("")
   st.sidebar.write("")
   if cp_sb == "Typical angina":
@@ -142,15 +142,15 @@ with tab1:
   # -- Value 3: non-anginal pain
   # -- Value 4: asymptomatic
 
-  trestbps = st.sidebar.number_input(label=":violet[**Resting blood pressure** (in mm Hg on admission to the hospital)]", min_value=df_final['trestbps'].min(), max_value=df_final['trestbps'].max())
-  st.sidebar.write(f":orange[Min] value: :orange[**{df_final['trestbps'].min()}**], :red[Max] value: :red[**{df_final['trestbps'].max()}**]")
+  trestbps = st.sidebar.number_input(label=":#4682B4[**Resting blood pressure** (in mm Hg on admission to the hospital)]", min_value=df_final['trestbps'].min(), max_value=df_final['trestbps'].max())
+  st.sidebar.write(f":orange[Min] value: :#B8860B[**{df_final['trestbps'].min()}**], :#E32227[Max] value: :red[**{df_final['trestbps'].max()}**]")
   st.sidebar.write("")
 
-  chol = st.sidebar.number_input(label=":violet[**Serum cholestoral** (in mg/dl)]", min_value=df_final['chol'].min(), max_value=df_final['chol'].max())
-  st.sidebar.write(f":orange[Min] value: :orange[**{df_final['chol'].min()}**], :red[Max] value: :red[**{df_final['chol'].max()}**]")
+  chol = st.sidebar.number_input(label=":#4682B4[**Serum cholestoral** (in mg/dl)]", min_value=df_final['chol'].min(), max_value=df_final['chol'].max())
+  st.sidebar.write(f":orange[Min] value: :#B8860B[**{df_final['chol'].min()}**], :#E32227[Max] value: :#E32227[**{df_final['chol'].max()}**]")
   st.sidebar.write("")
 
-  fbs_sb = st.sidebar.selectbox(label=":violet[**Fasting blood sugar > 120 mg/dl?**]", options=["False", "True"])
+  fbs_sb = st.sidebar.selectbox(label=":#4682B4[**Fasting blood sugar > 120 mg/dl?**]", options=["False", "True"])
   st.sidebar.write("")
   st.sidebar.write("")
   if fbs_sb == "False":
@@ -160,7 +160,7 @@ with tab1:
   # -- Value 0: false
   # -- Value 1: true
 
-  restecg_sb = st.sidebar.selectbox(label=":violet[**Resting electrocardiographic results**]", options=["Normal", "Having ST-T wave abnormality", "Showing left ventricular hypertrophy"])
+  restecg_sb = st.sidebar.selectbox(label=":#4682B4[**Resting electrocardiographic results**]", options=["Normal", "Having ST-T wave abnormality", "Showing left ventricular hypertrophy"])
   st.sidebar.write("")
   st.sidebar.write("")
   if restecg_sb == "Normal":
@@ -173,11 +173,11 @@ with tab1:
   # -- Value 1: having ST-T wave abnormality (T wave inversions and/or ST  elevation or depression of > 0.05 mV)
   # -- Value 2: showing probable or definite left ventricular hypertrophy by Estes' criteria
 
-  thalach = st.sidebar.number_input(label=":violet[**Maximum heart rate achieved**]", min_value=df_final['thalach'].min(), max_value=df_final['thalach'].max())
-  st.sidebar.write(f":orange[Min] value: :orange[**{df_final['thalach'].min()}**], :red[Max] value: :red[**{df_final['thalach'].max()}**]")
+  thalach = st.sidebar.number_input(label=":#4682B4[**Maximum heart rate achieved**]", min_value=df_final['thalach'].min(), max_value=df_final['thalach'].max())
+  st.sidebar.write(f":orange[Min] value: :#B8860B[**{df_final['thalach'].min()}**], :#E32227[Max] value: :red[**{df_final['thalach'].max()}**]")
   st.sidebar.write("")
 
-  exang_sb = st.sidebar.selectbox(label=":violet[**Exercise induced angina?**]", options=["No", "Yes"])
+  exang_sb = st.sidebar.selectbox(label=":#4682B4[**Exercise induced angina?**]", options=["No", "Yes"])
   st.sidebar.write("")
   st.sidebar.write("")
   if exang_sb == "No":
@@ -187,8 +187,8 @@ with tab1:
   # -- Value 0: No
   # -- Value 1: Yes
 
-  oldpeak = st.sidebar.number_input(label=":violet[**ST depression induced by exercise relative to rest**]", min_value=df_final['oldpeak'].min(), max_value=df_final['oldpeak'].max())
-  st.sidebar.write(f":orange[Min] value: :orange[**{df_final['oldpeak'].min()}**], :red[Max] value: :red[**{df_final['oldpeak'].max()}**]")
+  oldpeak = st.sidebar.number_input(label=":#4682B4[**ST depression induced by exercise relative to rest**]", min_value=df_final['oldpeak'].min(), max_value=df_final['oldpeak'].max())
+  st.sidebar.write(f":orange[Min] value: :#B8860B[**{df_final['oldpeak'].min()}**], :#E32227[Max] value: :red[**{df_final['oldpeak'].max()}**]")
   st.sidebar.write("")
 
   data = {
@@ -216,6 +216,7 @@ with tab1:
   result = ":violet[-]"
 
   predict_btn = st.button("**Predict**", type="primary")
+  predict_btn.button_style = "background-color: #191970; color: white; border-radius: 5px;"
 
   st.write("")
   if predict_btn:
